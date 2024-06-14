718969e7ed52fe17948636afbb52a9c7306dc590
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-06-14 23:36:52 +0600

    django orm

--- about_us/models.py
+++ about_us/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+class Student(models.Model):
+    stu_id = models.IntegerField()
+    stu_name = models.CharField(max_length=50)
+    stu_email = models.EmailField(max_length=30)
+    
+    def __str__(self):
+        return f"{self.stu_name} {self.stu_email}"
+    
